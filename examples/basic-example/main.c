307a85360110b90ae96ed228952cdb85b1a15ecd
Stephen M. Cameron <stephenmcameron@gmail.com>
2026-06-27 16:22:41 -0400

    Make basic example program actually do something
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- examples/basic-example/main.c
+++ examples/basic-example/main.c
@@ -1,10 +1,274 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <SDL2/SDL.h>
 
 #define SNIS_TYPEFACE_DECLARE_GLOBALS
 #include <snis-renderer.h> 
 #undef SNIS_TYPEFACE_DECLARE_GLOBALS
 
+static int real_screen_width, real_screen_height;
+static int time_to_quit = 0;
+static struct mesh *unitcube;
+static struct entity_context *ecx;
+static struct entity *box;
+union quat box_orientation = IDENTITY_QUAT_INITIALIZER; 
+
+static struct material box_material;
+
+static int start_sdl(void)
+{
+	SDL_SetHint(SDL_HINT_NO_SIGNAL_HANDLERS, "1"); /* Suppress default SDL2 SIGTERM and SIGINT signal handlers */
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+		fprintf(stderr, "Unable to initialize SDL (Video):  %s\n", SDL_GetError());
+		return 1;
+	}
+	if (SDL_Init(SDL_INIT_EVENTS) != 0) {
+		fprintf(stderr, "Unable to initialize SDL (Events):  %s\n", SDL_GetError());
+		return 1;
+	}
+	atexit(SDL_Quit);
+	return 0;
+}
+
+static void set_sdl_attributes(void)
+{
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 5);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 5);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 5);
+	/* SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16); */
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 0);
+	SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 0);
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+}
+
+static unsigned int load_texture(char *filename, int linear_colorspace)
+{
+	char fname[PATH_MAX + 1];
+
+	snprintf(fname, sizeof(fname), "%s/%s", ".", filename);
+	return graph_dev_load_texture(fname, linear_colorspace);
+}
+
+static void load_texture_mapped_material(struct material *m, char *diffuse_file, char *emit_file)
+{
+	material_init_texture_mapped(m);
+	m->texture_mapped.texture_id = load_texture(diffuse_file, 0);
+	if (emit_file)
+		m->texture_mapped.emit_texture_id = load_texture(emit_file, 0);
+}
+
+static void load_textures(void)
+{
+	static int loaded = 0;
+
+	if (loaded)
+		return;
+
+	load_texture_mapped_material(&box_material, "textures/box-diffuse.png", NULL);
+	loaded = 1;
+}
+
+static void init_meshes(void)
+{
+	unitcube = mesh_unit_cube(20);
+	mesh_unit_cube_uv_map(unitcube);
+	mesh_graph_dev_init(unitcube);
+}
+
+static int configure_window(SDL_Window *window)
+{
+	SDL_GL_GetDrawableSize(window, &real_screen_width, &real_screen_height);
+	sng_set_screen_size(real_screen_width, real_screen_height);
+
+	static int gl_is_setup = 0;
+	if (!gl_is_setup) {
+		graph_dev_setup("./shaders");
+		gl_is_setup = 1;
+	}
+
+	load_textures();
+
+	static int meshes_loaded = 0;
+	if (!meshes_loaded) {
+		init_meshes();
+		meshes_loaded = 1;
+	}
+	return 1;
+}
+
+static int key_press_cb(SDL_Window *window, SDL_Keysym *keysym, int key_repeat)
+{
+	switch (keysym->sym) {
+		case SDLK_ESCAPE:
+			time_to_quit = 1;
+			break;
+		default:
+			break;
+	}
+}
+
+static int key_release_cb(SDL_Keysym *keysym)
+{
+}
+
+static void handle_window_event(SDL_Window *window, SDL_Event event)
+{
+	int width, height;
+
+	switch (event.window.event) {
+	case SDL_WINDOWEVENT_RESIZED:
+	case SDL_WINDOWEVENT_SIZE_CHANGED:
+		break;
+	default:
+		break;
+	}
+}
+
+static void process_events(SDL_Window *window)
+{
+	SDL_Event event;
+
+	/* Grab all the events off the queue. */
+	while (SDL_PollEvent(&event)) {
+		switch (event.type) {
+		case SDL_KEYDOWN:
+			key_press_cb(window, &event.key.keysym, event.key.repeat);
+			break;
+		case SDL_KEYUP:
+			key_release_cb(&event.key.keysym);
+			break;
+		case SDL_QUIT:
+			/* By default SDL2 installs signal handlers for SIGINT and SIGTERM and converts
+			 * both of them into SDL_QUIT events.  We suppress these default signal handlers
+			 * and install our own, so we should not get here except by e.g. clicking the 'X'
+			 * on the window border.
+			 */
+			time_to_quit = 1;
+			break;
+		case SDL_WINDOWEVENT:
+			handle_window_event(window, event);
+			break;
+		case SDL_MOUSEBUTTONDOWN:
+			/* handle_button_press(&event.button); */
+			break;
+		case SDL_MOUSEBUTTONUP:
+			/* handle_button_release(&event.button); */
+			break;
+		case SDL_MOUSEMOTION:
+			/* handle_motion_notify(window, &event.motion); */
+			break;
+		case SDL_MOUSEWHEEL:
+			/* handle_mouse_scroll(&event.wheel); */
+			break;
+		}
+	}
+}
+
+static void move_objects(void)
+{
+	static int timer = 0;
+	union quat orientation;
+
+	quat_init_axis(&orientation, 0, 1, 0, (timer % 360) * M_PI / 180.0);
+	update_entity_orientation(box, &orientation);
+	timer++;
+}
+
+static void draw_window(SDL_Window *window)
+{
+	graph_dev_start_frame();
+	render_entities(ecx);
+	graph_dev_end_frame();
+	glFinish();
+	SDL_GL_SwapWindow(window);
+}
+
 int main(__attribute__((unused)) int argc, __attribute__((unused))  char *argv[])
 {
+	struct snis_object_pool *pool;
+	static int SCREEN_WIDTH = 800;
+	static int SCREEN_HEIGHT = 600;
+
+	snis_object_pool_setup(&pool, 100);
+	if (!pool) {
+		fprintf(stderr, "Failed to allocated object pool, exiting.\n");
+		exit(1);
+	}
+
+	ecx = entity_context_new(100, 100);
+	if (start_sdl()) {
+		fprintf(stderr, "Failed to start SDL, exiting\n");
+		exit(1);
+	}
+
+	set_sdl_attributes();
+
+	SDL_Window *window = SDL_CreateWindow("snis-renderer Basic Example Program",
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+		0, 0, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIDDEN);
+	if (!window) {
+		fprintf(stderr, "Could not create window: %s\n", SDL_GetError());
+		exit(1);
+	}
+	SDL_SetWindowSize(window, SCREEN_WIDTH, SCREEN_HEIGHT);
+	sng_set_clip_window(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+	SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+	(void) gl_context;
+
+	configure_window(window);
+	SDL_ShowWindow(window);
+	graph_dev_clear_window();
+	SDL_GL_SwapWindow(window);
+
+	double currentTime = time_now_double();
+	double nextTime = currentTime + 1.0 / 60.0;
+
+	const float angle_of_view = 30.0 * M_PI / 180.0;
+	const float near = 1.0;
+	const float far = 5000.0;
+	union vec3 camera_up = { {0, 1, 0}, };
+	union vec3 camera_pos = { { -4.0, 4.0, 0 }, };
+	union vec3 camera_lookat = { { 0, 0, 0 } };
+
+	camera_assign_up_direction(ecx, camera_up.v.x, camera_up.v.y, camera_up.v.z);	
+	camera_set_pos(ecx, camera_pos.v.x, camera_pos.v.y, camera_pos.v.z);
+	camera_look_at(ecx, camera_lookat.v.x, camera_lookat.v.y, camera_lookat.v.z);
+	set_renderer(ecx, FLATSHADING_RENDERER);
+	camera_set_parameters(ecx, near, far, SCREEN_WIDTH, SCREEN_HEIGHT, angle_of_view); 
+	calculate_camera_transform(ecx);
+	entity_context_set_hi_lo_poly_pixel_threshold(ecx, 200.0);
+
+	set_lighting(ecx, 20.0, 20.0, 20.0);
+	set_ambient_light(ecx, 0.35);
+
+	box = add_entity(ecx, unitcube, 0, 0, 0, WHITE);
+	update_entity_material(box, &box_material);
+	update_entity_orientation(box, &box_orientation);
+
+	while (1) {
+		currentTime = time_now_double();
+		if (currentTime >= nextTime) {
+			process_events(window);
+			move_objects();
+			draw_window(window);
+			currentTime = time_now_double();
+			nextTime = currentTime + 1.0 / 60.0;	
+		} else {
+			double timeToSleep = nextTime - currentTime;
+			if (timeToSleep > 0)
+				sleep_double(timeToSleep);
+		}
+		if (time_to_quit) {
+			/* probably should do some clean up */
+			break;	
+		}
+	}
+	entity_context_free(ecx);
 	return 0;
 }
---
+++ renderer/material.c
@@ -0,0 +1,285 @@
+/*
+	Copyright (C) 2010 Jeremy Van Grinsven
+	Author: Jeremy Van Grinsven
+
+	This file is part of Spacenerds In Space.
+
+	Spacenerds in Space is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	Spacenerds in Space is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Spacenerds in Space; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+
+#include "mtwist.h"
+#include "quat.h"
+#include "snis_graph.h"
+#include "material.h"
+#include "graph_dev.h"
+
+static unsigned int load_texture(const char *asset_dir, char *filename, int linear_colorspace)
+{
+	char fname[PATH_MAX + 1];
+
+	snprintf(fname, sizeof(fname), "%s/textures/%s", asset_dir, filename);
+	return graph_dev_load_texture(fname, linear_colorspace);
+}
+
+static const char *gnu_basename(const char *path)
+{
+	char *base = strrchr(path, '/');
+	return base ? base + 1 : path;
+}
+
+static const char *get_texture_filename(unsigned int texture_id)
+{
+	return gnu_basename(graph_dev_get_texture_filename(texture_id));
+}
+
+int material_nebula_read_from_file(const char *asset_dir, const char *filename,
+					struct material *nebula)
+{
+	FILE *f;
+	int rc;
+	int i;
+	char full_filename[PATH_MAX + 1];
+	struct material_nebula *mt = &nebula->nebula;
+
+	nebula->type = MATERIAL_NEBULA;
+	nebula->billboard_type = MATERIAL_BILLBOARD_TYPE_NONE;
+	nebula->rotate_randomly = 0;
+	sprintf(full_filename, "%s/materials/%s", asset_dir, filename);
+
+	f = fopen(full_filename, "r");
+	if (!f) {
+		fprintf(stderr, "material_nebula: Error opening file '%s'\n", full_filename);
+		return -1;
+	}
+
+	for (i = 0; i < MATERIAL_NEBULA_NPLANES; i++) {
+		char texture_filename[PATH_MAX+1];
+		rc = fscanf(f, "texture %s\n", texture_filename);
+		if (rc != 1) {
+			fprintf(stderr, "material_nebula: Error reading 'texture' from file '%s'\n", full_filename);
+			fclose(f);
+			return -1;
+		}
+		mt->texture_id[i] = load_texture(asset_dir, texture_filename, 1);
+
+		rc = fscanf(f, "orientation %f %f %f %f\n", &mt->orientation[i].q.q0, &mt->orientation[i].q.q1,
+			&mt->orientation[i].q.q2, &mt->orientation[i].q.q3);
+		if (rc != 4) {
+			fprintf(stderr, "material_nebula: Error reading 'orientation' from file '%s'\n", full_filename);
+			fclose(f);
+			return -1;
+		}
+	}
+
+	rc = fscanf(f, "alpha %f\n", &mt->alpha);
+	if (rc != 1) {
+		fprintf(stderr, "material_nebula: Error reading 'alpha' from file '%s'\n", full_filename);
+		fclose(f);
+		return -1;
+	}
+
+	rc = fscanf(f, "tint %f %f %f\n", &mt->tint.red, &mt->tint.green, &mt->tint.blue);
+	if (rc != 3) {
+		fprintf(stderr, "material_nebula: Error reading 'tint' from file '%s'\n", full_filename);
+		fclose(f);
+		return -1;
+	}
+
+	fclose(f);
+	return 0;
+}
+
+int material_nebula_write_to_file(const char *asset_dir, const char *filename,
+					struct material *nebula)
+{
+	FILE *f;
+	int i;
+	char full_filename[PATH_MAX + 1];
+	struct material_nebula *mt = &nebula->nebula;
+
+	sprintf(full_filename, "%s/materials/%s", asset_dir, filename);
+
+	f = fopen(full_filename, "w");
+	if (!f) {
+		fprintf(stderr, "material_nebula: Error opening file '%s' to write\n", full_filename);
+		return -1;
+	}
+
+	for (i = 0; i < MATERIAL_NEBULA_NPLANES; i++) {
+		const char *texture_filename = get_texture_filename(mt->texture_id[i]);
+		fprintf(f, "texture %s\n", texture_filename);
+		fprintf(f, "orientation %f %f %f %f\n", mt->orientation[i].q.q0, mt->orientation[i].q.q1,
+			mt->orientation[i].q.q2, mt->orientation[i].q.q3);
+	}
+
+	fprintf(f, "alpha %f\n", mt->alpha);
+	fprintf(f, "tint %f %f %f\n", mt->tint.red, mt->tint.green, mt->tint.blue);
+
+	fclose(f);
+
+	return 0;
+}
+
+void material_init_texture_mapped(struct material *m)
+{
+	m->type = MATERIAL_TEXTURE_MAPPED;
+	m->billboard_type = MATERIAL_BILLBOARD_TYPE_NONE;
+	m->texture_mapped.texture_id = 0;
+	m->texture_mapped.emit_texture_id = 0;
+	m->texture_mapped.normalmap_id = 0;
+	m->texture_mapped.emit_intensity = 1.0;
+	m->texture_mapped.specular_power = 512.0;
+	m->texture_mapped.specular_intensity = 0.2;
+	m->rotate_randomly = 0;
+}
+
+void material_init_texture_mapped_unlit(struct material *m)
+{
+	m->type = MATERIAL_TEXTURE_MAPPED_UNLIT;
+	m->billboard_type = MATERIAL_BILLBOARD_TYPE_NONE;
+	m->texture_mapped_unlit.texture_id = 0;
+	m->texture_mapped_unlit.do_cullface = 1;
+	m->texture_mapped_unlit.do_blend = 0;
+	m->texture_mapped_unlit.alpha = 1.0;
+	m->texture_mapped_unlit.tint = sng_get_color(WHITE);
+	m->rotate_randomly = 0;
+}
+
+void material_init_texture_cubemap(struct material *m)
+{
+	m->type = MATERIAL_TEXTURE_CUBEMAP;
+	m->billboard_type = MATERIAL_BILLBOARD_TYPE_NONE;
+	m->texture_cubemap.texture_id = 0;
+	m->texture_cubemap.do_cullface = 1;
+	m->texture_cubemap.do_blend = 0;
+	m->texture_cubemap.alpha = 1.0;
+	m->texture_cubemap.tint = sng_get_color(WHITE);
+	m->rotate_randomly = 0;
+}
+
+void material_init_nebula(struct material *m)
+{
+	m->type = MATERIAL_NEBULA;
+	m->billboard_type = MATERIAL_BILLBOARD_TYPE_NONE;
+	int i;
+	for (i = 0; i < MATERIAL_NEBULA_NPLANES; i++) {
+		m->nebula.texture_id[i] = 0;
+		m->nebula.orientation[i] = identity_quat;
+	}
+	m->nebula.alpha = 1.0;
+	m->nebula.tint = sng_get_color(WHITE);
+	m->rotate_randomly = 0;
+}
+
+void material_init_textured_particle(struct material *m)
+{
+	m->type = MATERIAL_TEXTURED_PARTICLE;
+	m->billboard_type = MATERIAL_BILLBOARD_TYPE_NONE;
+	m->textured_particle.texture_id = 0;
+	m->textured_particle.radius = 1.0;
+	m->textured_particle.time_base = 1.0;
+	m->rotate_randomly = 0;
+}
+
+void material_init_textured_planet(struct material *m)
+{
+	m->type = MATERIAL_TEXTURED_PLANET;
+	m->billboard_type = MATERIAL_BILLBOARD_TYPE_NONE;
+	m->textured_planet.texture_id = 0;
+	m->textured_planet.ring_material = 0;
+	m->textured_planet.water_color_r = 0.1;
+	m->textured_planet.water_color_g = 0.33;
+	m->textured_planet.water_color_b = 1.0;
+	m->textured_planet.sun_color_r = 1.0;
+	m->textured_planet.sun_color_g = 1.0;
+	m->textured_planet.sun_color_b = 0.7;
+	m->rotate_randomly = 0;
+}
+
+void material_init_textured_shield(struct material *m)
+{
+	m->type = MATERIAL_TEXTURED_SHIELD;
+	m->billboard_type = MATERIAL_BILLBOARD_TYPE_NONE;
+	m->textured_shield.texture_id = 0;
+	m->rotate_randomly = 0;
+}
+
+void material_init_atmosphere(struct material *m)
+{
+	m->type = MATERIAL_ATMOSPHERE;
+	m->billboard_type = MATERIAL_BILLBOARD_TYPE_NONE;
+	m->atmosphere.r = 81.0f / 255.0f;
+	m->atmosphere.g = 81.0f / 255.0f;
+	m->atmosphere.b = 1.0f;
+	m->atmosphere.scale = 1.03f;
+	m->atmosphere.brightness = NULL;
+	m->rotate_randomly = 0;
+}
+
+void material_init_alpha_by_normal(struct material *m)
+{
+	m->type = MATERIAL_ALPHA_BY_NORMAL;
+	m->billboard_type = MATERIAL_BILLBOARD_TYPE_NONE;
+	m->alpha_by_normal.texture_id = -1; /* texture is optional for alpha by normal */
+	m->alpha_by_normal.do_cullface = 1;
+	m->alpha_by_normal.texture_id = 0;
+	m->alpha_by_normal.invert = 0;
+	m->alpha_by_normal.alpha = 1.0;
+	m->alpha_by_normal.tint = sng_get_color(WHITE);
+	m->rotate_randomly = 0;
+}
+
+void material_init_textured_planet_ring(struct material *m)
+{
+	m->type = MATERIAL_TEXTURED_PLANET_RING;
+	m->billboard_type = MATERIAL_BILLBOARD_TYPE_NONE;
+	m->textured_planet_ring.texture_id = 0;
+	m->textured_planet_ring.alpha = 1.0;
+	m->textured_planet_ring.texture_v = 0.0f;
+	m->textured_planet_ring.tint = sng_get_color(WHITE);
+	m->rotate_randomly = 0;
+}
+
+void material_init_wireframe_sphere_clip(struct material *m)
+{
+	m->type = MATERIAL_WIREFRAME_SPHERE_CLIP;
+	m->billboard_type = MATERIAL_BILLBOARD_TYPE_NONE;
+	m->wireframe_sphere_clip.center = 0;
+	m->wireframe_sphere_clip.radius = 0;
+	m->wireframe_sphere_clip.radius_fade = 0;
+	m->rotate_randomly = 0;
+}
+
+void material_init_planetary_lightning(struct material *m)
+{
+	m->type = MATERIAL_PLANETARY_LIGHTNING;
+	m->billboard_type = MATERIAL_BILLBOARD_TYPE_NONE;
+	m->planetary_lightning.texture_id = 0;
+	m->rotate_randomly = 0;
+}
+
+void material_init_warp_gate_effect(struct material *m)
+{
+	m->type = MATERIAL_WARP_GATE_EFFECT;
+	m->billboard_type = MATERIAL_BILLBOARD_TYPE_NONE;
+	m->warp_gate_effect.texture_id = 0;
+	m->warp_gate_effect.u1 = 0.0;
+	m->warp_gate_effect.u2 = 0.01;
+	m->rotate_randomly = 0;
+}
