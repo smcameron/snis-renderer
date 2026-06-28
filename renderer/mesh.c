6d337c45296e6e61a7c3b720de24b92b9b0637b9
Stephen M. Cameron <stephenmcameron@gmail.com>
2026-06-27 22:59:49 -0400

    Fix mesh_unit_cube() to produce cubes 1 unit on a side
    It was producing cubes that were 2 units to a side.
    
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- renderer/mesh.c
+++ renderer/mesh.c
@@ -206,7 +206,8 @@ void mesh_derelict(struct mesh *m, float distortion)
 	mesh_graph_dev_init(m);
 }
 
-void mesh_scale(struct mesh *m, float scale)
+
+static void mesh_scale_helper(struct mesh *m, float scale)
 {
 	int i;
 
@@ -216,6 +217,11 @@ void mesh_scale(struct mesh *m, float scale)
 		m->v[i].z *= scale;
 	}
 	m->radius = mesh_compute_radius(m);
+}
+
+void mesh_scale(struct mesh *m, float scale)
+{
+	mesh_scale_helper(m, scale);
 	mesh_graph_dev_init(m);
 }
 
@@ -1627,6 +1633,7 @@ struct mesh *mesh_unit_cube(int subdivisions)
 	mesh_set_flat_shading_vertex_normals(m);
 	m->nlines = 0;
 	m->radius = mesh_compute_radius(m);
+	mesh_scale_helper(m, 0.5); /* Scale cube so each side is 1 unit */
 	mesh_graph_dev_init(m);
 	return m;
 bail:
